60ca4c07505f01fcd61c0827363f4afe44007b45
Vincent Wainman <vincewainman@gmail.com>
2022-10-16 19:15:02 -0600

    add logic for read toggle and removing a book

--- library.js
+++ library.js
@@ -2,28 +2,84 @@
 
 const defaultBook1 = new Book("The Fellowship of the Ring", "J. R. R. Tolkien", 479, "Read");
 const defaultBook2 = new Book("The Two Towers", "J. R. R. Tolkien", 352, "Read");
-let library = [defaultBook1, defaultBook2];
+const library = [];
+addBookToLibrary(defaultBook1);
+addBookToLibrary(defaultBook2);
+const bookForm = document.querySelector("#new-book-form");
+const MIN_PAGES = 1;
+const MAX_PAGES = 99999;
+const newBookBtn = document.querySelector("#new-book-btn");
+newBookBtn.onclick = toggleBookForm;
 
 function Book(title, author, pages, isRead) {
     this.title = title
     this.author = author
     this.pages = pages
-    this.isRead = isRead
+    this.status = isRead ? "Read" : "Not Read";
 }
 
 function addBookToLibrary(book) {
+    insertHTMLTableRow("library", book, library.length);
     library.push(book);
 }
 
-function updateTable() {
-
+function insertHTMLTableRow(tableId, object, index) {
+    const table = document.getElementById(tableId);
+    const tbody = table.getElementsByTagName('tbody')[0];
+    const row = tbody.insertRow(index);
+    row.setAttribute("data-index-number", index);
+    insertSanitizedRowCells(row, object, ["title", "author", "pages"]);
 }
 
+function insertSanitizedRowCells(row, object, userInputKeys) {
+    for (const key of userInputKeys) {
+        const td = row.insertCell(-1);
+        td.setHTML(object[key]);
+    }
+
+    appendButtonCell(row, object["status"], "book-status", toggleReadStatus);
+    appendButtonCell(row, "Remove", "delete-book", removeBook);
+}
+
+// remove the selected book from the libary
+function removeBook() {
+    console.log(this);
+    const bookRow = this.closest('tr');
+    console.log(bookRow);
+    const rowIndex = Number(bookRow.dataset.indexNumber);
+    // update the html table and library array
+    library.splice(rowIndex, 1);
+    const table = document.getElementById("library");
+    const tbody = table.getElementsByTagName('tbody')[0];
+    tbody.deleteRow(rowIndex);
+    // inefficient update to the data index number
+    // TODO: refactor
+    for (let i = 0, row; row = tbody.rows[i]; i++) {
+        row.setAttribute("data-index-number", i);
+    }
+
+}
+// toggle the read status on the selected book
+function toggleReadStatus() {
+    const rowIndex = this.closest('tr').dataset.indexNumber;
+    const newStatus = (this.textContent === "Read") ? "Not Read" : "Read";
+    library[rowIndex].status = newStatus;
+    this.textContent = newStatus;
+}
+
+function appendButtonCell(row, text, classValue, onclickEvent) {
+    const td = row.insertCell(-1);
+    const btn = document.createElement("button");
+    btn.setAttribute("class", classValue);
+    btn.textContent = `${text}`;
+    btn.onclick = onclickEvent;
+    td.append(btn);
+}
+
+// show/hide new book form
 function toggleBookForm() {
     let bookForm = document.getElementById("new-book-form");
     let newBookBtn = document.getElementById("new-book-btn");
-    console.log(bookForm.style.display);
-    console.log(bookForm);
     if (bookForm.style.display === "none") {
         bookForm.style.display = "grid";
         newBookBtn.textContent = "Hide Form"
@@ -33,5 +89,37 @@ function toggleBookForm() {
     }
 }
 
-// use SetHTML() to sanitize the text before insertion
-// when inserting plain text, use Node.textContent
+
+function hasNonEmptyString(input) {
+    return (typeof input === "string" && input.trim() !== "")
+}
+
+function hasNumberBetween(input, min, max) {
+    return (typeof input === "number" && input >= min && input <= max);
+}
+
+function hasBool(value) {
+    return typeof value === "boolean";
+}
+
+// form validation and data handling
+bookForm.addEventListener("submit", (e) => {
+    // stop form submission
+    e.preventDefault();
+    // validate form elements
+    const title = bookForm["title"].value;
+    const titleValid = hasNonEmptyString(title);
+    const author = bookForm["author"].value;
+    const authorValid = hasNonEmptyString(author);
+    const pages = Number(bookForm["pages"].value);
+    const pagesValid = hasNumberBetween(pages, MIN_PAGES, MAX_PAGES);
+    const read = bookForm["read"].checked;
+    const readValid = hasBool(read);
+    if (titleValid && authorValid && pagesValid && readValid) {
+        // create and add book to library on valid inputs
+        const newBook = new Book(title, author, pages, read);
+        addBookToLibrary(newBook);
+    } else {
+        alert("Invalid input");
+    }
+});
